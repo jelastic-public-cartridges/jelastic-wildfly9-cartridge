58784b471f5589263aaf1b99ebe064248b7057be
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2017-09-21 19:31:20 +0300

    Update deploy.sh

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -4,21 +4,51 @@
 
 WGET=$(which wget);
 
+source /etc/jelastic/environment
+
+
+function ensureFileCanBeDownloaded(){
+    local resource_url=$1;
+    resource_data_dize=$($CURL -s --head $resource_url | $GREP "Content-Length" | $AWK -F ":" '{ print $2 }'| $SED 's/[^0-9]//g');
+    freebytesleft=$(( 1024 *  $(df  | $GREP "/$" | $AWK '{ print $4 }' | head -n 1)-1024*1024));
+    [ -z ${resource_data_dize} ] && return 0;
+    [ ${resource_data_dize} -lt  ${freebytesleft} ] || { writeJSONResponseErr "result=>4075" "message=>No free diskspace"; die -q; }
+    return 0;
+}
+
+function getPackageName() {
+    if [ -f "$package_url" ]; then
+        echo test1
+        package_name="$package_url";
+    elif [[ "${package_url}" =~ file://* ]]; then
+        package_name="${package_url:7}"
+        [ -f "$package_name" ] || { writeJSONResponseErr "result=>4078" "message=>Error loading file from URL"; die -q; }
+    else
+        ensureFileCanBeDownloaded $package_url;
+        $WGET --no-check-certificate --content-disposition --directory-prefix="$download_dir" $package_url >> $ACTIONS_LOG 2>&1 || { writeJSONResponseErr "result=>4078" "message=>Error loading file from URL"; die -q; }
+        package_name="${download_dir}/$(ls ${download_dir})";
+        [ ! -s "$package_name" ] && {
+            set -f
+            rm -f "${package_name}";
+            set +f
+            writeJSONResponseErr "result=>4078" "message=>Error loading file from URL";
+            die -q;
+        }
+    fi
+}
+
 function _deploy(){
      [ "x${context}" == "xroot" ] && context="ROOT";
      [ -f "${WEBROOT}/${context}.war" ] && rm -f ${WEBROOT}/${context}.war;
      [ -f "${WEBROOT}/${context}.ear" ] && rm -f ${WEBROOT}/${context}.ear;
      [ -f "${WEBROOT}/${context}.war.undeployed" ] && mv ${WEBROOT}/${context}.war.undeployed ${WEBROOT}/${context}.war.deployed
      [ -f "${WEBROOT}/${context}.ear.undeployed" ] && mv ${WEBROOT}/${context}.ear.undeployed ${WEBROOT}/${context}.ear.deployed
-     local download_dir=$(mktemp -d)
-     $WGET --no-check-certificate --content-disposition -P "$download_dir" "$package_url";
-     local app_filename=$(ls -1 "$download_dir")
-     cd "$download_dir";
+     download_dir=$(mktemp -d)
+     getPackageName
      set +f;
-     [[ "$app_filename" =~ (.*.ear) ]] && mv -f "${app_filename}" "${context}.ear" || mv -f "${app_filename}" "${context}.war"
-     chown jelastic:jelastic ${context}.*;
-     mv ${context}.* ${WEBROOT};
-     cd /; rm -rf "$download_dir";
+     chown jelastic:jelastic "${package_name}"
+     [[ "${package_name}" =~ (.*.ear) ]] && cp -f "${package_name}" ${WEBROOT}/"${context}.ear" || cp -f "${package_name}" ${WEBROOT}/"${context}.war"
+     rm -rf ${download_dir}
      set -f;
 }
 
