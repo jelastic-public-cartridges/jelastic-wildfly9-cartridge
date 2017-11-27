e82ce904a4e8d6f613c3f238cb5a33515335224a
Dmytro Zubelevych <dz@jelastic.com>
2017-11-27 11:52:57 +0200

    JE-35374 Cannot open admin panel after restart of environment

--- jelastic/scripts/firewall.sh
+++ jelastic/scripts/firewall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
-/sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4949 -j REDIRECT --to-ports 9990;
-/sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4848 -j REDIRECT --to-ports 9993;
+#/sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4949 -j REDIRECT --to-ports 9990;
+#/sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4848 -j REDIRECT --to-ports 9993;
 /sbin/iptables -t nat -D PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080;
