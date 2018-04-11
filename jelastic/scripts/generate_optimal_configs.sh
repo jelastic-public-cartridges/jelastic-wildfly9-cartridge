c8c26f95c9b6df843b112c8907589af910dde9c0
Dmytro Zubelevych <dz@jelastic.com>
2018-04-11 10:49:23 +0300

    JE-38583 [Deploy] An error occurred while deploying the project jem compute deploy -a false -c ROOT -u file

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo -e "$(find $(realpath /usr/java/latest) -name libjli.so -printf "%h\n")" > /etc/ld.so.conf.d/java.conf ; \
-ldconfig
+if [[ "$UID" == '0' ]]; then
+    echo -e "$(find $(realpath /usr/java/latest) -name libjli.so -printf "%h\n")" > /etc/ld.so.conf.d/java.conf ; \
+    ldconfig
 
-JAVABIN=$(which java 2>/dev/null)
-[ ! -z "$JAVABIN" ] && {
-    setcap 'cap_net_bind_service=+ep' $(readlink -f "$JAVABIN")
-}
+    JAVABIN=$(which java 2>/dev/null)
+    [ ! -z "$JAVABIN" ] && {
+        setcap 'cap_net_bind_service=+ep' $(readlink -f "$JAVABIN")
+    }
+fi
